d32db9cbb51f2bbcca6d4abf3d50a4977e98a2a1
Patrick Marty <marty.patrick@gmail.com>
2015-04-11 23:49:15 +0200

    bugfix on empty word when constructing trie

--- src/automaton/trie.py
+++ src/automaton/trie.py
@@ -7,13 +7,14 @@ from fst import MutableFST
 def build_trie( words , fst_factory=MutableFST ):
     automaton = fst_factory()
     for word in words:
-        currentState = automaton.get_initial_state()
-        for letter in word:
-            targetState = automaton.get_target( currentState , letter )
-            if targetState is None:
-                targetState = automaton.add_state()
-                automaton.add_transition( currentState , letter , targetState )
-            currentState = targetState
-        automaton.set_final_state( currentState )
-        automaton.add_output( currentState , word )
+        if len( word ) != 0:
+            currentState = automaton.get_initial_state()
+            for letter in word:
+                targetState = automaton.get_target( currentState , letter )
+                if targetState is None:
+                    targetState = automaton.add_state()
+                    automaton.add_transition( currentState , letter , targetState )
+                currentState = targetState
+            automaton.set_final_state( currentState )
+            automaton.add_output( currentState , word )
     return automaton
--- test/test_trie.py
+++ test/test_trie.py
@@ -9,14 +9,25 @@ from automaton.trie import build_trie
 
 
 class BuildTrieTestCase( TestCase ):
+    def test_emptyword( self ):
+        words = [ '' ]
+        t = build_trie( words )
+        self.assertEqual( t.to_dict() , { 'initial':0 , 'finals':[] , 'transitions':{ 0:{} } } )
+
+
     def test_a( self ):
         words = [ 'a' ]
         t = build_trie( words )
-        print t.to_dict()
         self.assertEqual( t.to_dict() , { 'initial':0 , 'finals':[ 1 ] , 'outputs':{ 1:[ 'a' ] } , 'transitions':{ 0:{ 'a':1 } , 1:{} } } )
         self.assertFalse( t.accept( '' ) )
         self.assertTrue( t.accept( 'a' ) )
         self.assertFalse( t.accept( 'aa' ) )
+
+
+    def test_a_and_emptyword( self ):
+        words = [ 'a' , '' ]
+        t = build_trie( words )
+        self.assertEqual( t.to_dict() , { 'initial':0 , 'finals':[ 1 ] , 'outputs':{ 1:[ 'a' ] } , 'transitions':{ 0:{ 'a':1 } , 1:{} } } )
 
 
     def test_a_b( self ):
